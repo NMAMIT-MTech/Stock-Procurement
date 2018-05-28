fc21af2354307f9727344626f572c76eb6b2c488
NMAMIT-MTech <37292759+NMAMIT-MTech@users.noreply.github.com>
2018-05-28 16:46:56 +0530

    Create StockService.java

---
+++ StockService.java
@@ -0,0 +1,13 @@
+package com.java.service;
+
+import com.java.pojo.Stock;
+
+public interface StockService {
+
+	public void addStock(Stock stock);
+
+	public void editStock(Stock stock);
+
+	public void deleteStock(int id);
+
+}
